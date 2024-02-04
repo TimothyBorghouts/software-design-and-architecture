c319b0fd03925617209997c9445fc23fd7999789
Timothy Borghouts <tcr.borghouts@student.avans.nl>
2024-02-04 11:54:33 +0100

    Trying to deploy command application to azure web service

---
+++ FunctionApp/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace software_design_and_architecture_3_colleges
+{
+    public class Movie
+    {
+        private string _title;
+        private List<MovieScreening> _movieScreening;
+
+        public Movie(string title)
+        {
+            _title = title;
+            _movieScreening = new List<MovieScreening>();
+        }
+
+        public void AddScreening(MovieScreening screening)
+        {
+            _movieScreening.Add(screening);
+        }
+
+        public override string ToString()
+        {
+            return "Movie: " + _title;
+        }
+    }
+}
---
+++ FunctionApp/MovieTicket.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualBasic;
+
+namespace software_design_and_architecture_3_colleges
+{
+    public class MovieTicket
+    {
+        private int _rowNr;
+        private int _seatNr;
+        private bool _isPremium;
+        private MovieScreening _movieScreening;
+
+        public int Id { get; internal set; }
+
+        public MovieTicket(int rowNr, int seatNr, bool isPremium, MovieScreening movieScreening)
+        {
+            _rowNr = rowNr;
+            _seatNr = seatNr;
+            _isPremium = isPremium;
+            _movieScreening = movieScreening;
+        }
+
+        public MovieTicket(int rowNr, int seatNr, bool isPremium)
+        {
+            _rowNr = rowNr;
+            _seatNr = seatNr;
+            _isPremium = isPremium;
+        }
+
+        public bool IsPremium()
+        {
+            return _isPremium;
+        }
+
+        public double GetPrice()
+        {
+            return _movieScreening.GetPricePerSeat();
+        }
+
+        public bool IsMidWeek()
+        {
+            DayOfWeek wk = _movieScreening.GetDateTime().DayOfWeek;
+            if (wk == DayOfWeek.Monday || wk == DayOfWeek.Tuesday || wk == DayOfWeek.Wednesday || wk == DayOfWeek.Thursday)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            string ticket = "Ticket:\nRow: " + _rowNr + "\nSeat Number: " + _seatNr;
+            if (_isPremium)
+            {
+                ticket += "\nPremium ticket";
+            }
+            return ticket;
+        }
+    }
+}
