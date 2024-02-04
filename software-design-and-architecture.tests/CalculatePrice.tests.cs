c663c15c69985ae8846b6d1207088c489e00fc83
Matthijs Feringa <mt.feringa@student.avans.nl>
2024-02-04 22:21:10 +0100

    fixed some tests and added some more

--- software-design-and-architecture.tests/CalculatePrice.tests.cs
+++ software-design-and-architecture.tests/CalculatePrice.tests.cs
@@ -9,8 +9,8 @@ namespace software_design_and_architecture.tests
             Order order = new Order(1, isStudent);
             Movie movie = new Movie("Timothy de draak");
             MovieScreening movieScreening = new MovieScreening(movie, screeningDateTime, 10);
-            MovieTicket movieTicket1 = new MovieTicket(2, 5, isPremium);
-            MovieTicket movieTicket2 = new MovieTicket(2, 5, isPremium);
+            MovieTicket movieTicket1 = new MovieTicket(2, 5, isPremium, movieScreening);
+            MovieTicket movieTicket2 = new MovieTicket(2, 5, isPremium, movieScreening);
             order.AddSeatReservation(movieTicket1);
             order.AddSeatReservation(movieTicket2);
             return order;
@@ -30,20 +30,6 @@ namespace software_design_and_architecture.tests
             Assert.Equal("No movie tickets available. Cannot calculate total price.", exception.Message);
         }
 
-        //2 Tickets with no premium, no 2e ticket free, no discounts. 2 x 10 = 20,-
-        [Fact]
-        public void NoDiscountOrFreeTickets()
-        {
-            //Arrange
-            Order order = CreateOrderWithMovieTickets(false, new DateTime(2024, 2, 4), false);
-
-            //Act
-            var result = order.CalculatePrice();
-
-            // Assert
-            Assert.Equal(20, result);
-        }
-
         // 2 tickets and 2e ticket is free because of student order 1 x 10 = 10,-
         [Fact]
         public void StudentSecondTicketFree()
@@ -58,7 +44,7 @@ namespace software_design_and_architecture.tests
             Assert.Equal(10, result);
         }
 
-        // 2 tickets and 2e ticket is free because it is in midweek 1 x 10 = 10,-
+        // 2 tickets and 2e ticket is free because it is it is a monday 1 x 10 = 10,-
         [Fact]
         public void MidWeekSecondTicketFree()
         {
@@ -72,16 +58,70 @@ namespace software_design_and_architecture.tests
             Assert.Equal(10, result);
         }
 
+        // 2 tickets and 2e ticket is free because it is it is a tuesdays 1 x 10 = 10,-
+        [Fact]
+        public void MidWeekSecondTicketFreeTuesday()
+        {
+            //Arrange
+            Order order = CreateOrderWithMovieTickets(false, new DateTime(2024, 2, 6), false);
+            //Act
+            var result = order.CalculatePrice();
+            // Assert
+            Assert.Equal(10, result);
+        }
+
+        // 2 tickets and 2e ticket is free because it is it is a wednesday 1 x 10 = 10,-
+        [Fact]
+        public void MidWeekSecondTicketFreeWednesday()
+        {
+            //Arrange
+            Order order = CreateOrderWithMovieTickets(false, new DateTime(2024, 2, 7), false);
+            //Act
+            var result = order.CalculatePrice();
+            // Assert
+            Assert.Equal(10, result);
+        }
+
+        // 2 tickets and 2e ticket is free because it is it is a thursday 1 x 10 = 10,-
+        [Fact]
+        public void MidWeekSecondTicketFreeThursday()
+        {
+            //Arrange
+            Order order = CreateOrderWithMovieTickets(false, new DateTime(2024, 2, 8), false);
+            //Act
+            var result = order.CalculatePrice();
+            // Assert
+            Assert.Equal(10, result);
+        }
+
+        //2 Tickets with no premium, no 2e ticket free, no discounts. 2 x 10 = 20,-
+        [Fact]
+        public void NoDiscountOrFreeTickets()
+        {
+            //Arrange
+            Order order = CreateOrderWithMovieTickets(false, new DateTime(2024, 2, 4), false);
+
+            //Act
+            var result = order.CalculatePrice();
+
+            // Assert
+            Assert.Equal(20, result);
+        }
+
         // 6 tickets with 10% group discount 6 x 10 = 60 x 0.9 = 54,-
         [Fact]
         public void GroupDiscount()
         {
             //Arrange
             Order order = CreateOrderWithMovieTickets(false, new DateTime(2024, 2, 4), false);
-            order.AddSeatReservation(new MovieTicket(2, 5, false));
-            order.AddSeatReservation(new MovieTicket(2, 5, false));
-            order.AddSeatReservation(new MovieTicket(2, 5, false));
-            order.AddSeatReservation(new MovieTicket(2, 5, false));
+            Movie movie = new Movie("Timothy de draak"); 
+            DateTime screeningDateTime = new DateTime(2024, 2, 4);
+            MovieScreening movieScreening = new MovieScreening(movie, screeningDateTime, 10);
+
+            order.AddSeatReservation(new MovieTicket(2, 5, false, movieScreening));
+            order.AddSeatReservation(new MovieTicket(2, 5, false, movieScreening));
+            order.AddSeatReservation(new MovieTicket(2, 5, false, movieScreening));
+            order.AddSeatReservation(new MovieTicket(2, 5, false, movieScreening));
 
             //Act
             var result = order.CalculatePrice();
@@ -90,18 +130,83 @@ namespace software_design_and_architecture.tests
             Assert.Equal(54, result);
         }
 
-        // 2 premium tickets without discounts or free tickets 12 x 2 = 24,-
+        // 6 tickets with 10% group discount and 2e ticket free 6 x 10 = 60 - 3 x 10 = 30 x 0.9 = 27,-
         [Fact]
-        public void PremiumTicketFee()
+        public void GroupDiscountAndSecondTicketFree()
         {
             //Arrange
-            Order order = CreateOrderWithMovieTickets(false, new DateTime(2024, 2, 5), true);
+            Order order = CreateOrderWithMovieTickets(false, new DateTime(2024, 2, 5), false);
+            Movie movie = new Movie("Timothy de draak");
+            DateTime screeningDateTime = new DateTime(2024, 2, 5);
+            MovieScreening movieScreening = new MovieScreening(movie, screeningDateTime, 10);
+
+            order.AddSeatReservation(new MovieTicket(2, 5, false, movieScreening));
+            order.AddSeatReservation(new MovieTicket(2, 5, false, movieScreening));
+            order.AddSeatReservation(new MovieTicket(2, 5, false, movieScreening));
+            order.AddSeatReservation(new MovieTicket(2, 5, false, movieScreening));
 
             //Act
             var result = order.CalculatePrice();
 
             // Assert
-            Assert.Equal(24, result);
+            Assert.Equal(27, result);
+        }
+
+        // 5 tickets will not give 10% discount
+        [Fact]
+        public void NoGroupDiscount()
+        {
+            //Arrange
+            Order order = CreateOrderWithMovieTickets(false, new DateTime(2024, 2, 4), false);
+            Movie movie = new Movie("Timothy de draak");
+            DateTime screeningDateTime = new DateTime(2024, 2, 4);
+            MovieScreening movieScreening = new MovieScreening(movie, screeningDateTime, 10);
+
+            order.AddSeatReservation(new MovieTicket(2, 5, false, movieScreening));
+            order.AddSeatReservation(new MovieTicket(2, 5, false, movieScreening));
+            order.AddSeatReservation(new MovieTicket(2, 5, false, movieScreening));
+            //Act
+            var result = order.CalculatePrice();
+            // Assert
+            Assert.Equal(50, result);
+        }
+
+        // 2 premium tickets without discounts or free tickets 13 x 2 = 26,-
+        [Fact]
+        public void PremiumTicketFee()
+        {
+            //Arrange
+            Order order = CreateOrderWithMovieTickets(false, new DateTime(2024, 2, 4), true);
+
+            //Act
+            var result = order.CalculatePrice();
+
+            // Assert
+            Assert.Equal(26, result);
+        }
+
+        // 2 premium tickets with 2e ticket free 13,-
+        [Fact]
+        public void PremiumTicketFeeSecondTicketFree()
+        {
+            //Arrange
+            Order order = CreateOrderWithMovieTickets(false, new DateTime(2024, 2, 5), true);
+            //Act
+            var result = order.CalculatePrice();
+            // Assert
+            Assert.Equal(13, result);
+        }
+
+        // 2 premium student tickets with 2e ticket free 12,-
+        [Fact]
+        public void PremiumStudentTicketFeeSecondTicketFree()
+        {
+            //Arrange
+            Order order = CreateOrderWithMovieTickets(true, new DateTime(2024, 2, 5), true);
+            //Act
+            var result = order.CalculatePrice();
+            // Assert
+            Assert.Equal(12, result);
         }
     }
 }
